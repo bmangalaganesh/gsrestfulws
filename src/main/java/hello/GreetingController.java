110e801df826e0429c8ab1f6300a3f2dfffbd8cb
manglu <manglu@gmail.com>
2016-07-27 09:51:23 +1000

    Simple Restful service that I worked together with Bals on 26th July 2016

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,28 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import javax.servlet.annotation.HttpMethodConstraint;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+	
+	
+	  private static final String template = "Hello, %s!";
+	
+	    private final AtomicLong counter = new AtomicLong();
+
+	    @RequestMapping(value="/greeting",method={RequestMethod.POST,RequestMethod.GET},produces=MediaType.APPLICATION_XML_VALUE)
+	    public Greeting greeting(@RequestParam(value="newparamname", defaultValue="World") String name) {
+	        return new Greeting(counter.incrementAndGet(),
+	                            String.format(template, name));
+	    }
+
+}
