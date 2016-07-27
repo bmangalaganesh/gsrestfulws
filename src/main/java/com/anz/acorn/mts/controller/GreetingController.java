7ff13218ca2a315393b1f09a7cf93a5dd74c815d
manglu <manglu@gmail.com>
2016-07-27 17:29:33 +1000

    moved the Application and GreetingController to the controller package as the controllers are only picked up from the same package as the "Application class". I will have to figure out why this is the case

--- src/main/java/com/anz/acorn/mts/controller/GreetingController.java
+++ src/main/java/com/anz/acorn/mts/controller/GreetingController.java
@@ -1,4 +1,4 @@
-package hello;
+package com.anz.acorn.mts.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import hello.Greeting;
+
 @RestController
 public class GreetingController {
 	
@@ -15,6 +17,9 @@ public class GreetingController {
 	  private static final String template = "Hello, %s!";
 	
 	    private final AtomicLong counter = new AtomicLong();
+	    
+	    private Greeting aGreeting = null;
+	    
 
 	    @RequestMapping(value="/greeting",method={RequestMethod.POST,RequestMethod.GET},produces= {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
 	    public Greeting greeting(@RequestParam(value="newparamname", defaultValue="World") String name) {
@@ -22,4 +27,8 @@ public class GreetingController {
 	                            String.format(template, name));
 	    }
 
+
+	    
+
+	    
 }
