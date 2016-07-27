7d79c9fb69689a2bcf4e39a1ba3eb582e8749d01
manglu <manglu@gmail.com>
2016-07-27 13:22:30 +1000

    supports content negotiation. The produce now specifies the array  - JSON_VALUE and XML_VALUE

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -2,13 +2,10 @@ package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-import javax.servlet.annotation.HttpMethodConstraint;
-
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,7 +16,7 @@ public class GreetingController {
 	
 	    private final AtomicLong counter = new AtomicLong();
 
-	    @RequestMapping(value="/greeting",method={RequestMethod.POST,RequestMethod.GET},produces=MediaType.APPLICATION_XML_VALUE)
+	    @RequestMapping(value="/greeting",method={RequestMethod.POST,RequestMethod.GET},produces= {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
 	    public Greeting greeting(@RequestParam(value="newparamname", defaultValue="World") String name) {
 	        return new Greeting(counter.incrementAndGet(),
 	                            String.format(template, name));
