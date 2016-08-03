170cf8c1ea6d60ba2542ae7d13a419327bafc18e
manglu <manglu@gmail.com>
2016-08-03 23:09:38 +1000

    attempted to use the hateoas support

---
+++ src/main/java/com/anz/acorn/mts/domain/Address.java
@@ -0,0 +1,15 @@
+package com.anz.acorn.mts.domain;
+
+public class Address {
+
+	private String streetName;
+
+	public String getStreetName() {
+		return streetName;
+	}
+
+	public void setStreetName(String streetName) {
+		this.streetName = streetName;
+	}
+
+}
